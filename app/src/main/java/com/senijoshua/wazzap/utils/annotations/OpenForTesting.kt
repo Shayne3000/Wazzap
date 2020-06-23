881f06e8bbb9dac2e7ed18058045afeca5457d56
Seni Joshua <alfred3000prime@hotmail.com>
2020-06-23 01:01:31 -0400

    text correction

--- app/src/main/java/com/senijoshua/wazzap/utils/annotations/OpenForTesting.kt
+++ app/src/main/java/com/senijoshua/wazzap/utils/annotations/OpenForTesting.kt
@@ -3,7 +3,7 @@ package com.senijoshua.wazzap.utils.annotations
 /**
  * This annotation changes the modifier of a kotlin class
  * from final to open in order to mock Kotlin classes in unit tests.
- * 
+ *
  * @author Seni Joshua
  */
 @Retention(AnnotationRetention.SOURCE)
