86333a974e9a535697ca4fff726f4ece448c00c2
Seni Joshua <alfred3000prime@hotmail.com>
2020-06-22 23:04:01 -0400

    added precommit hooks

---
+++ app/src/main/java/com/senijoshua/wazzap/scripts/installHooks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This script will install a pre-commit hook that will run any and all
+# .hook files in this directory. It will also install dependencies, if any
+# required by these hooks.
+#
+
+cd "$(git rev-parse --show-toplevel)/scripts" || return
+
+mkdir ../.git/scripts/
+echo "Copying pre-commit to git hooks"
+cp -rp pre-commit ../.git/scripts/pre-commit
+if [ $? -eq 0 ]; then
+    echo "...Success!"
+else
+    echo "...Failed!"
+    exit 1
+fi
+
+# Check if all dependencies have been installed
+
+echo ""
+echo "Integrating ktlint with Android Studio..."
+"$(git config --get ktlint.jar)" --android applyToIDEA -y
---
+++ app/src/main/java/com/senijoshua/wazzap/scripts/uninstallHooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This script will delete all files created by install.sh
+# and will uninstall dependencies.
+
+cd "$(git rev-parse --show-toplevel)" || return
+
+echo "Removing pre-commit hook"
+rm -rf .git/scripts/pre-commit
+if [ $? -ne 0 ]; then
+    echo "! Failed to remove pre-commit hook. You will have to do so manually."
+fi
