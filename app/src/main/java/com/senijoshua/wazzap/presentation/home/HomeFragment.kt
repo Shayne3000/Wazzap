ee6c9df13c56e574665f915aa7f362b7d18034c6
Seni Joshua <alfred3000prime@hotmail.com>
2020-10-31 01:49:08 -0400

    mild cleanup

--- app/src/main/java/com/senijoshua/wazzap/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/senijoshua/wazzap/presentation/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.senijoshua.wazzap.presentation.home
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -22,6 +21,7 @@ private const val TAB_COUNT = 3
 private const val CHAT_INDEX = 0
 private const val STATUS_INDEX = 1
 private const val CALLS_INDEX = 2
+
 /**
  * Fragment that is the first point of entry into the app and holds all the primary fragments
  * in a Viewpager.
@@ -30,10 +30,9 @@ private const val CALLS_INDEX = 2
  */
 class HomeFragment : WazzapFragment(R.layout.fragment_home) {
     @Inject lateinit var viewModel: HomeViewModel
-    private val homeTabTitleResources = listOf(R.string.chats, R.string.status, R.string.calls)
-    val rootActivity: WazzapActivity by lazy {
-        activity as WazzapActivity
-    }
+
+    private val homeTabTitles = listOf(R.string.chats, R.string.status, R.string.calls)
+    val rootActivity: WazzapActivity by lazy { activity as WazzapActivity }
     val pagerAdapter: HomeViewPagerAdapter by lazy {
         HomeViewPagerAdapter(childFragmentManager, lifecycle)
     }
@@ -57,15 +56,17 @@ class HomeFragment : WazzapFragment(R.layout.fragment_home) {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // sets the toolbar view as the action bar for this fragment
         rootActivity.setSupportActionBar(home_toolbar)
 
         home_view_pager.adapter = pagerAdapter
 
+        // initialise the tablayout and attach the view pager to it
         TabLayoutMediator(home_tab_layout, home_view_pager) { tab, position ->
             when (position) {
-                CHAT_INDEX -> tab.text = getString(homeTabTitleResources[CHAT_INDEX])
-                STATUS_INDEX -> tab.text = getString(homeTabTitleResources[STATUS_INDEX])
-                CALLS_INDEX -> tab.text = getString(homeTabTitleResources[CALLS_INDEX])
+                CHAT_INDEX -> tab.text = getString(homeTabTitles[CHAT_INDEX])
+                STATUS_INDEX -> tab.text = getString(homeTabTitles[STATUS_INDEX])
+                CALLS_INDEX -> tab.text = getString(homeTabTitles[CALLS_INDEX])
             }
             // when a certain tab is selected, the viewpager would page to that tab
             home_view_pager.currentItem = tab.position
