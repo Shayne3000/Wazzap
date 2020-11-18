4ee3b30dcd820d8ab3ae28a9377f57886ded828b
Seni Joshua <alfred3000prime@hotmail.com>
2020-11-17 23:48:29 -0500

    started adding conversation menu items

--- app/src/main/java/com/senijoshua/wazzap/presentation/chat/ConversationFragment.kt
+++ app/src/main/java/com/senijoshua/wazzap/presentation/chat/ConversationFragment.kt
@@ -1,17 +1,34 @@
 package com.senijoshua.wazzap.presentation.chat
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import com.senijoshua.wazzap.R
+import com.senijoshua.wazzap.databinding.FragmentConversationBinding
 import com.senijoshua.wazzap.presentation.root.WazzapFragment
 
 class ConversationFragment : WazzapFragment(R.layout.fragment_conversation) {
 
+    private lateinit var layoutDataBinding: FragmentConversationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_fragment_conversation, menu)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        layoutDataBinding = FragmentConversationBinding.bind(view)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
     }
 }
